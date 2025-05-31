4831a6bdad92586038d5bcb2fa5c60bb140e2ffd
SamuelBlandon <samuelblandon1113128@correo.itm.edu.co>
2025-05-31 15:18:24 -0500

    Clase de repuestos

--- Clases/clsRepuesto.cs
+++ Clases/clsRepuesto.cs
@@ -22,7 +22,6 @@ namespace Servicios_Jue.Clases
             catch (Exception ex)
             {
                 return "Error al insertar el producto: " + ex.Message;
-                //holaaa
             }
         }
         public string Actualizar()
