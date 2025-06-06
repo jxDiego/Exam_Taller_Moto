96eaa04bf462102828f1891887dde07fac498c09
NELSON-PINZON\PC <nelsonpinzon328764@correo.itm.edu.co>
2025-06-05 20:26:08 -0500

    Lib Factura

---
+++ Models/libFacturaVenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam_Taller_Moto.Models
+{
+    public class FacturaDetalle
+    {
+        public FacturaVenta facturaVenta { get; set; }
+
+        public DetalleFacturaVenta detalle { get; set; }
+        
+    }
+}
